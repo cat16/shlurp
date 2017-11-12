3037c562e8f8ef19afd6b51f94f1e10e8f76a056
KhryptK <aaautube.28@gmail.com>
2017-11-12 12:43:21 -0500

    updated commands.js

--- commands.js
+++ commands.js
@@ -1,11 +1,11 @@
 module.exports = [
 
-    new Command(
-        "test",
-        "just testing to see if the bot will work on the most basic level",
-        (msg) => {
+    new Command({
+        name: "test",
+        description: "just testing to see if the bot will work on the most basic level",
+        run: (msg) => {
             console.log(msg.content);
         }
-    )
+    })
 
 ]
--- handler.js
+++ handler.js
@@ -1,3 +1,11 @@
+const config = require("./config.json");
+const commands = require("./commands.js");
+
 module.exports = function(msg){
-    
+    if(msg.startsWith(config.prefix, 1)){
+        msg = msg.replace(config.prefix, "")
+        for(let cmd in commands){
+            if()
+        }
+    }
 }
