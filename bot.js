48a4e3a2f6371150e578fcaaf21acccf854a8542
KhryptK <aaautube.28@gmail.com>
2017-11-12 12:18:56 -0500

    started handler

--- bot.js
+++ bot.js
@@ -1,8 +1,9 @@
 const Discord = require("discord.js");
 const bot = new Discord.Client();
+const handler = require("./handler.js");
 
-bot.on('message', () => {
-    //code
+bot.on('message', (msg) => {
+    
 });
 
 bot.login(/*token*/);
